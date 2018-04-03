cbdbdf82970ed99e952d3685d02d998e9f8f8f8f
Vinicius Souza <hi@vsouza.com>
2018-04-03 14:53:06 -0300

    remove wrong validation on Auth middleware

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -12,16 +12,11 @@ func AuthMiddleware() gin.HandlerFunc {
 		config := config.GetConfig()
 		reqKey := c.Request.Header.Get("X-Auth-Key")
 		reqSecret := c.Request.Header.Get("X-Auth-Secret")
-		key := config.GetString("http.auth.key")
-		secret := config.GetString("http.auth.secret")
-		if reqKey == "" || reqSecret == "" {
-			c.AbortWithStatus(401)
-			return
-		}
-		if key == "" || secret == "" {
+		if key := config.GetString("http.auth.key"); len(strings.TrimSpace(key)) == 0 {
 			c.AbortWithStatus(500)
-			log.Println("key and secret credentials not found on config file")
-			return
+		}
+		if secret := config.GetString("http.auth.secret"); len(strings.TrimSpace(secret)) == 0 {
+			c.AbortWithStatus(401)
 		}
 		if key != reqKey || secret != reqSecret {
 			c.AbortWithStatus(401)
