a7c614c93ce0983a46b355f19ec0fe531b49ec1e
Victor Kochkarev <victor.kochkarev@gmail.com>
2019-03-19 19:51:24 +0000

    Refactoring - used http status code enum

--- controllers/user.go
+++ controllers/user.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/vsouza/go-gin-boilerplate/models"
+	"net/http"
 )
 
 type UserController struct{}
@@ -13,14 +14,14 @@ func (u UserController) Retrieve(c *gin.Context) {
 	if c.Param("id") != "" {
 		user, err := userModel.GetByID(c.Param("id"))
 		if err != nil {
-			c.JSON(500, gin.H{"message": "Error to retrieve user", "error": err})
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "Error to retrieve user", "error": err})
 			c.Abort()
 			return
 		}
-		c.JSON(200, gin.H{"message": "User founded!", "user": user})
+		c.JSON(http.StatusOK, gin.H{"message": "User founded!", "user": user})
 		return
 	}
-	c.JSON(400, gin.H{"message": "bad request"})
+	c.JSON(http.StatusBadRequest, gin.H{"message": "bad request"})
 	c.Abort()
 	return
 }
